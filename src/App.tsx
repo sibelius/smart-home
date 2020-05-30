5a586efd64c646e37099890ea82a93af6152aa9f
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2020-05-30 18:57:31 -0300

    feat(design): reality

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,97 @@
 import React from 'react';
 import { Text, Flex } from 'rebass';
 import styled from 'styled-components';
-import yoga from './img/yoga-small.jpg';
-import { space } from 'styled-system';
+import { space, borders } from 'styled-system';
+import logo from './img/logo.png';
 
-const Img = styled.img`
-  max-width: 100%;
-  display: block;
+import { ReactComponent as RoomsIcon } from './icons/rooms.svg';
+import { ReactComponent as SecurityIcon } from './icons/security.svg';
+import { ReactComponent as StatisticsIcon } from './icons/statistics.svg';
+import { ReactComponent as MembersIcon } from './icons/members.svg';
+import { ReactComponent as BillingIcon } from './icons/billing.svg';
+
+const Card = styled(Flex)`
+  ${borders}
 `;
 
-const Background = styled.div`
-  background: url(${props => props.url}) no-repeat center center fixed;
-  background-size: cover;
-  
-  height: 100%; 
+const Sidebar = styled(Card)`
+  border-top-left-radius: 60px;
+  border-bottom-left-radius: 60px;
 `;
 
-const Circle = styled.div`
-  width: 150px;
-  height: 150px;
-  border-radius: 50%;
-  border-color: white;
-  font-size:25px;
-  color: #fff;
-  line-height: 150px;
-  text-align: center;
-  border: 5px solid white;
+const Content = styled(Card)`
+  border-radius: 60px;
+  position: relative;
+  right: 50px;
+`;
+
+const Logo = styled.img`
+  width: 60px;
+  height: 60px;  
   ${space}
-`
+`;
+
+type MenuItemProps = {
+  icon: React.ReactNode,
+  label: string,
+}
+const MenuItem = ({ icon, label }: MenuItemProps) => {
+  return (
+    <Flex flexDirection='column' mt='40px'>
+      <RoomsIcon />
+      <Text color='#FFF5F5' fontSize='11px' mt='10px'>
+        Rooms
+      </Text>
+    </Flex>
+  );
+};
 
 const App = () => {
   return (
-    <>
-      <Background url={yoga}>
-        <Flex pt='10px' justifyContent='center'>
-          <Text as='p' color='white' fontSize='16px'>(11) 94046-8989</Text>
-        </Flex>
-        <Flex mt='100px' justifyContent='center'>
-          <Circle>
-            YOGA
-          </Circle>
-        </Flex>
-        <Flex mt='80px' justifyContent='center'>
-          <Text as='p' fontSize='30px' color='white'>
-            Find your way to yoga lifestyle
-          </Text>
-        </Flex>
-        <Flex mt='400px'></Flex>
-      </Background>
-    </>
+    <Flex bg='#E7E7E7'>
+      <Flex flex={1} alignItems='center' justifyContent='center' mt='100px' mb='100px'>
+        <Card borderRadius='60px' height='905px' width='1200px' bg='#FFFFFF'>
+          <Sidebar bg='#65BDC0' width='140px' p='10px' flexDirection='column'>
+            <Logo src={logo} mt='30px' />
+            <Flex mt='100px' ml='10px' flexDirection='column'>
+              <Flex flexDirection='column' mt='40px'>
+                <RoomsIcon />
+                <Text color='#FFF5F5' fontSize='11px' mt='10px'>
+                  Rooms
+                </Text>
+              </Flex>
+              <Flex flexDirection='column' mt='40px'>
+                <SecurityIcon />
+                <Text color='#FFF5F5' fontSize='11px' mt='10px'>
+                  Security
+                </Text>
+              </Flex>
+              <Flex flexDirection='column' mt='40px'>
+                <StatisticsIcon />
+                <Text color='#FFF5F5' fontSize='11px' mt='10px'>
+                  Statistics
+                </Text>
+              </Flex>
+              <Flex flexDirection='column' mt='40px'>
+                <MembersIcon />
+                <Text color='#FFF5F5' fontSize='11px' mt='10px'>
+                  Members
+                </Text>
+              </Flex>
+              <Flex flexDirection='column' mt='40px'>
+                <BillingIcon />
+                <Text color='#FFF5F5' fontSize='11px' mt='10px'>
+                  Billing
+                </Text>
+              </Flex>
+            </Flex>
+          </Sidebar>
+          <Content bg='white' width='100%'>
+            <span>Content</span>
+          </Content>
+        </Card>
+      </Flex>
+    </Flex>
   );
 }
 
--- src/Root.tsx
+++ src/Root.tsx
@@ -6,22 +6,11 @@ import { createGlobalStyle,  ThemeProvider } from 'styled-components';
 
 const theme = createMuiTheme();
 
-const GlobalStyle = createGlobalStyle`
-  body {
-   font-family: 'Work Sans', sans-serif;
-  }
-  
-  h1, h2, h3, h4, h5, h6 {
-   font-family: 'Crimson Text', serif;
-  }
-`
-
 const Root = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <GlobalStyle />
         <App />
       </ThemeProvider>
     </MuiThemeProvider>
